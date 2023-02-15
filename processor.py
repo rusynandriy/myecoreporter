9e09ab91e3e06e18735fc16d8c0376882227ebdd
Andriy Rusyn <rusyn.andriy@gmail.com>
2023-02-15 13:34:30 -0600

    add processor to export chats and update completion_checker

--- processor.py
+++ processor.py
@@ -6,7 +6,6 @@ import os
 if __name__ == "__main__":
     # get all the chats
     conversations = dynamo.get_all_conversations()
-    print("conversations", conversations)
 
     # make a new folder for today's chats
     today = dt.now().strftime("%Y-%m-%d")
@@ -14,13 +13,9 @@ if __name__ == "__main__":
 
     # loop through them and save them to that folder
     for conversation in conversations["Items"]:
-        print("conversation", conversation)
         chat_log = conversation.get("conversation_text")
-        print(chat_log)
         username = conversation.get("username")
-        print(username)
         started_at = conversation.get("started_at")
-        print("started at", started_at)
         with open(f"{today}/{username}_{started_at}.txt", "w") as f:
             for line in chat_log:
                 f.write(line + "\n")
